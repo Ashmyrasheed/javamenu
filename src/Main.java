76790e6feca4308df00a12d48e5e8dab0f36e5fa
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-04 16:23:51 +0530

    second

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
@@ -21,37 +19,58 @@ public class Main {
             Scanner scanner=new Scanner(System.in);
             choice=scanner.nextInt();
 
-            switch (choice)
-            {
+            switch (choice) {
 
-            case 1:
-            System.out.println("insert student selected");
-            System.out.println("enter the name:");
-            String name = scanner.next();
-            System.out.println("enter the rollno:");
-            int rollno = scanner.nextInt();
-            System.out.println("enter the admno:");
-            int admno = scanner.nextInt();
-            System.out.println("enter the college name:");
-            String collegename = scanner.next();
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "");
-                String sql = "INSERT INTO `students`(`name`, `rollnumber`, `admno`, `college`) VALUES (?,?,?,?)";
-                PreparedStatement stmt = con.prepareStatement(sql);
-                stmt.setString(1, name);
-                stmt.setInt(2, rollno);
-                stmt.setInt(3, admno);
-                stmt.setString(4, collegename);
-                stmt.executeUpdate();
-            }
-            catch (Exception e){
-                System.out.println(e);
-            }
-            break;
-            case 2:
-                System.out.println("select student selected");
-                break;
+                case 1:
+                    System.out.println("insert student selected");
+                    System.out.println("enter the name:");
+                    String name = scanner.next();
+                    System.out.println("enter the rollno:");
+                    int rollno = scanner.nextInt();
+                    System.out.println("enter the admno:");
+                    int admno = scanner.nextInt();
+                    System.out.println("enter the college name:");
+                    String collegename = scanner.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "");
+                        String sql = "INSERT INTO `students`(`name`, `rollnumber`, `admno`, `college`) VALUES (?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setString(1, name);
+                        stmt.setInt(2, rollno);
+                        stmt.setInt(3, admno);
+                        stmt.setString(4, collegename);
+                        stmt.executeUpdate();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+                case 2:
+                    System.out.println("view student selected");
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "");
+                        String sql = "SELECT `name`, `rollnumber`, `admno`, `college` FROM `students`";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()) {
+                            String getName = rs.getString("name");
+                            String getRoll = rs.getString("rollnumber");
+                            String getAdm = rs.getString("admno");
+                            String getCollege = rs.getString("college");
+                            System.out.println("name="+getName);
+                            System.out.println("rollno="+getRoll);
+                            System.out.println("admno="+getAdm);
+                            System.out.println("college="+getCollege+"\n");
+                        }
+
+                    }
+
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
             case 3:
                 System.out.println("search student selected");
                 break;
